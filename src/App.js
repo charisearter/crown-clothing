f01215f6786515a1b1b0bee9e9d6d3d7564f11bf
Reese <arter.charise@gmail.com>
2021-05-30 11:39:09 -0500

    added login and register component page, added sign in page with basic form set up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Switch, Route } from "react-router";
 import "./App.scss";
 
-import Header from './components/header/Header'
+import SignInAndSignUpPage from "./pages/signIn-signUp/SignIn-SignUp.jsx";
+import Header from "./components/header/Header";
 import HomePage from "./pages/homepage/Homepage";
-import ShopPage from './pages/shop/Shop'
-
+import ShopPage from "./pages/shop/Shop";
 
 function App() {
 	return (
-    <div>
-      <Header />
+		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
